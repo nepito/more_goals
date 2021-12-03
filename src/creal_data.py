a92689445b5d180e8579d73ffec279afe11811a1
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-12-02 18:06:31 -0800

    👩🏿‍💻 Add functionality

--- src/creal_data.py
+++ src/creal_data.py
@@ -2,10 +2,24 @@ import pandas as pd
 
 PATH_DATA = "data/raw/strength_league_39_2020.csv"
 data = pd.read_csv(PATH_DATA)
+OUTPUT_PATH = "data/processed/strength_and_goals_39_2020.csv"
+
+def remove_columns(data):
+    df = data.drop(columns=["home_id", "away_id", "home", "away", "won"])
+    return df
+
+
+def add_total_goals(data):
+    df = data.assign(
+        total_goal=data.home + data.away
+    )
+    return df
 
 
 def main():
-    print(data)
+    cleaned_data = add_total_goals(data)
+    cleaned_data = remove_columns(cleaned_data)
+    cleaned_data.to_csv(OUTPUT_PATH)
 
 
 if __name__ == "__main__":
