73b1c53e3fb089b413e78f1e088231159ae8de4e
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-12-02 17:47:30 -0800

    🎨 Make format with black

--- src/creal_data.py
+++ src/creal_data.py
@@ -1,7 +1,12 @@
 import pandas as pd
+
 PATH_DATA = "data/raw/strength_league_39_2020.csv"
 data = pd.read_csv(PATH_DATA)
+
+
 def main():
     print(data)
+
+
 if __name__ == "__main__":
     main()
